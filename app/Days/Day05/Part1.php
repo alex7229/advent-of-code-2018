96ff57b4964d38e8658864910c7131bc4f7efe1c
tup1tsa <natorvano@gmail.com>
2019-07-16 16:05:19 +0300

    increase processing speed to 4 secs per 50 000 input

--- app/Days/Day05/Part1.php
+++ app/Days/Day05/Part1.php
@@ -15,19 +15,33 @@ class Part1 extends Solver
 
     public function solve(): string
     {
-        // in order to increase the speed of dividing, polymer is split into 1000 long string pieces
+        return $this->calculateResultedPolymerLength($this->file);
+    }
+
+    protected function calculateResultedPolymerLength(string $polymerInput): int
+    {
+        // in order to increase the speed of dividing, polymer is split into 100 long string pieces
         // each of them is processed
         // all of the pieces are glued back together
+        // procedure is repeated once more, but dividing into 1000 long pieces (numbers are guessed)
         // polymer is processed one more time
-        $chunks = explode(",", chunk_split($this->file, 1000, ","));
+        $chunks = explode(",", chunk_split($polymerInput, 100, ","));
         $chunks = array_map(function ($polymer) {
             $processor = new PolymerProcessor(str_split($polymer));
             return $processor->shrink();
         }, $chunks);
-        $polymer = join("", $chunks);
-        $processor = new PolymerProcessor(str_split($polymer));
+        $currentPolymer = join("", $chunks);
+
+        $chunks = explode(",", chunk_split($currentPolymer, 1000, ","));
+        $chunks = array_map(function ($polymer) {
+            $processor = new PolymerProcessor(str_split($polymer));
+            return $processor->shrink();
+        }, $chunks);
+
+        $currentPolymer = join('', $chunks);
+
+        $processor = new PolymerProcessor(str_split($currentPolymer));
         return strlen($processor->shrink());
     }
-
 
 }
