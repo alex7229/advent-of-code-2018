49b2237bea36a89b30e76b48787fcf28c9f5e72d
tup1tsa <natorvano@gmail.com>
2019-07-17 16:08:46 +0300

    classes are autoladed by the string pattern

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once('vendor/autoload.php');
 
 
 if (!isset($argv[1])) {
-    throw new Exception('first argument should be day (for example, 1)');
+    throw new Exception('first argument should be day (for example, 01)');
 }
 
 if (!isset($argv[2])) {
@@ -14,42 +14,8 @@ if (!isset($argv[2])) {
 $day = $argv[1];
 $part = $argv[2];
 
-$solver = null;
-
-if ($day === '1' && $part === '1') {
-    $solver = new \App\Days\Day01\Part1();
-}
-if ($day === '1' && $part === '2') {
-    $solver = new \App\Days\Day01\Part2();
-}
-if ($day === '2' && $part === '1') {
-    $solver = new \App\Days\Day02\Part1();
-}
-if ($day === '2' && $part === '2') {
-    $solver = new \App\Days\Day02\Part2();
-}
-if ($day === '3' && $part === '1') {
-    $solver = new \App\Days\Day03\Part1();
-}
-if ($day === '4' && $part === '1') {
-    $solver = new \App\Days\Day04\Part1();
-}
-if ($day === '4' && $part === '2') {
-    $solver = new \App\Days\Day04\Part2();
-}
-if ($day === '5' && $part === '1') {
-    $solver = new \App\Days\Day05\Part1();
-}
-if ($day === '5' && $part === '2') {
-    $solver = new \App\Days\Day05\Part2();
-}
-if ($day === '6' && $part === '1') {
-    $solver = new \App\Days\Day06\Part1();
-}
-
-if (is_null($solver)) {
-    throw new Exception('Day or part is not implemented or input is not valid');
-}
+$class = '\App\Days\Day' . $day . '\Part' . $part;
+$solver = new $class();
 
 echo $solver->solve();
 echo "\n";
