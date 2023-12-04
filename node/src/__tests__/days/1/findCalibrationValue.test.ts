6ec049398dc64a19df43f63bb16b05e31330404e
Alex <natorvano@gmail.com>
2023-12-04 11:13:00 +0200

    fail day 1, part2

--- node/src/__tests__/days/1/findCalibrationValue.test.ts
+++ node/src/__tests__/days/1/findCalibrationValue.test.ts
@@ -6,3 +6,13 @@ it("should find calibration value for provided strings", () => {
   expect(findCalibrationValue("a1b2c3d4e5f")).toBe(15);
   expect(findCalibrationValue("treb7uchet")).toBe(77);
 });
+
+it("should find calibration value for provided strings including actual values like `one`, `two`, etc", () => {
+  expect(findCalibrationValue("two1nine", true)).toBe(29);
+  expect(findCalibrationValue("eightwothree", true)).toBe(83);
+  expect(findCalibrationValue("abcone2threexyz", true)).toBe(13);
+  expect(findCalibrationValue("xtwone3four", true)).toBe(24);
+  expect(findCalibrationValue("4nineeightseven2", true)).toBe(42);
+  expect(findCalibrationValue("zoneight234", true)).toBe(14);
+  expect(findCalibrationValue("7pqrstsixteen", true)).toBe(76);
+});
--- node/src/days/1/findCalibrationValue.ts
+++ node/src/days/1/findCalibrationValue.ts
@@ -1,7 +1,25 @@
-export type FindCalibrationValue = (input: string) => number;
+export type FindCalibrationValue = (
+  input: string,
+  parseNumbers?: boolean
+) => number;
 
-const findCalibrationValue: FindCalibrationValue = (input) => {
-  const digits = input.replace(/([^\d])/g, "");
+const findCalibrationValue: FindCalibrationValue = (
+  input,
+  parseNumbers = false
+) => {
+  let actualInput = input;
+  if (parseNumbers) {
+    actualInput = actualInput.replace(/(one)/g, "1");
+    actualInput = actualInput.replace(/(two)/g, "2");
+    actualInput = actualInput.replace(/(three)/g, "3");
+    actualInput = actualInput.replace(/(four)/g, "4");
+    actualInput = actualInput.replace(/(five)/g, "5");
+    actualInput = actualInput.replace(/(six)/g, "6");
+    actualInput = actualInput.replace(/(seven)/g, "7");
+    actualInput = actualInput.replace(/(eight)/g, "8");
+    actualInput = actualInput.replace(/(nine)/g, "9");
+  }
+  const digits = actualInput.replace(/([^\d])/g, "");
   if (digits.length === 0) {
     throw new Error("Cannot parse line for calibration");
   }
